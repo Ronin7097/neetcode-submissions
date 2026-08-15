e776ad3aefdd4675fef20ad25cac42aa19be884b
Divy Agarwal <f20230658@pilani.bits-pilani.ac.in>
2026-08-15 18:06:42 +0530

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    arr:list(int)=[int(i) for i in input().split(',')]
+    return arr[0] + arr[1]
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
