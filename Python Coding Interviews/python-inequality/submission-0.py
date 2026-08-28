e1af39f1f3030792dc17cb63122d184b14082bab
Divy Agarwal <f20230658@pilani.bits-pilani.ac.in>
2026-08-28 23:28:02 +0530

    Add: python-inequality - submission-0

---
+++ Python Coding Interviews/python-inequality/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def is_arr_valid(names: List[str], max_length: int) -> bool:
+    return any(0 < len(x) <= max_length for x in names)
+
+
+
+# do not modify below this line
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 3))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 2))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 0))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 1))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 4))
